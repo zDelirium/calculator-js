604a3f1054943ab0fb50f3931feeb2908c14aab0
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-01 11:28:23 -0500

    Added fixtures to test file

--- src/calc.test.js
+++ src/calc.test.js
@@ -1,5 +1,23 @@
 import  { sum, subtract } from "./calc";
 
+// Fixtures
+beforeAll(() => {
+    console.log("This will run before all the tests")
+});
+
+beforeEach(() => {
+    console.log("This will printed before each test");
+});
+
+afterEach(() => {
+    console.log("This will be printed after each test")
+});
+
+afterAll(() => {
+    console.log("This will run after all the tests")
+});
+
+
 // Create a test function to be run
 // using an expect 
 test('Adding 2 + 3 should give me 5', () => {
