4dcf726cc465ae34fb57d5c0d61dd67fa79668a2
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-01 11:22:16 -0500

    Added subtract function and test

--- src/calc.js
+++ src/calc.js
@@ -1,4 +1,5 @@
 const sum  = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 
 // Can export functions outside of this file
-export {sum}; //export default sum; could be used too. This is more specific
+export {sum, subtract}; //export default sum; could be used too. This is more specific
--- src/calc.test.js
+++ src/calc.test.js
@@ -1,7 +1,11 @@
-import  { sum } from "./calc";
+import  { sum, subtract } from "./calc";
 
 // Create a test function to be run
 // using an expect 
 test('Adding 2 + 3 should give me 5', () => {
     expect(sum(2, 3)).toBe(5);
-})
+});
+
+test('Substract 2 from 3 that should give me 1', () => {
+    expect(subtract(3, 2)).toBe(1);
+});
